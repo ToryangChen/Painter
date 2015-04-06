c02693f31e2f23937f90122374492ce53a040234
waxxl <991022948@qq.com>
2015-04-07 04:32:20 +0800

    Rejected commit: Default

--- app/src/main/java/com/nmid/painterdemo/GuessFragment.java
+++ app/src/main/java/com/nmid/painterdemo/GuessFragment.java
@@ -5,14 +5,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import com.nmid.painterdemo.com.nmid.GuessLVAdapter;
 
 /**
  * Created by Toryang on 2015/3/30.
  */
 public class GuessFragment extends Fragment {
+    ListView guessLV = null;
+    GuessLVAdapter guessLVAdapter =null;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.activity_guess,container,false);
+        View rootView = inflater.inflate(R.layout.activity_guess,container,false);
+        guessLV = (ListView)rootView.findViewById(R.id.guessLV);
+        guessLVAdapter = new GuessLVAdapter(inflater);
+        guessLV.setAdapter(guessLVAdapter);
+        return rootView;
     }
+
 }

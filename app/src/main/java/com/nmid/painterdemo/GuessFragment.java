bc24ba54a95c2da6bc54a412aca030abb9a489e0
ToryangChen <ToryangChen@outlook.com>
2015-04-18 19:17:50 +0800

    update

--- app/src/main/java/com/nmid/painterdemo/GuessFragment.java
+++ app/src/main/java/com/nmid/painterdemo/GuessFragment.java
@@ -8,23 +8,23 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 
-import com.nmid.painterdemo.com.nmid.GuessLVAdapter;
+//import com.nmid.painterdemo.com.nmid.GuessLVAdapter;
 
 /**
  * Created by Toryang on 2015/3/30.
  */
 public class GuessFragment extends Fragment {
     ListView guessLV = null;
-    GuessLVAdapter guessLVAdapter =null;
+    //GuessLVAdapter guessLVAdapter =null;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.activity_guess,container,false);
-        guessLV = (ListView)rootView.findViewById(R.id.guessLV);
-        guessLVAdapter = new GuessLVAdapter(inflater);
-        guessLV.setAdapter(guessLVAdapter);
+//        guessLV = (ListView)rootView.findViewById(R.id.guessLV);
+//        guessLVAdapter = new GuessLVAdapter(inflater);
+//        guessLV.setAdapter(guessLVAdapter);
         return rootView;
     }
 
